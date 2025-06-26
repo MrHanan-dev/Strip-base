010bfe3b0e4888433e2a0a6f0574cf5f1cdf465a
Muhammad Hanan <150981817+MrHanan-dev@users.noreply.github.com>
2025-06-26 17:49:48 +1200

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,3 +1,3 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-09-30.acacia' })
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-02-24.acacia' })
